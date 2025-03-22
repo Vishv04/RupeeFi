e431c7f297a449a79f14afa6aaaebc8023880209
vishv0407 <vishvboda0407@gmail.com>
2025-03-22 13:02:23 +0530

    added fixes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,19 +1,12 @@
 import { useState } from 'react'
+import Login from './components/auth/Login'
 import './App.css'
-import Navbar from './components/Navbar'
-import AuthForm from './components/AuthForm'
-import { GoogleOAuthProvider } from '@react-oauth/google'
 
 function App() {    
   return (
-    <GoogleOAuthProvider clientId="YOUR_GOOGLE_CLIENT_ID">
-      <div className="min-h-screen bg-gray-950">
-        <Navbar />
-        <div className="pt-24 px-4 min-h-screen flex flex-col items-center">
-          <AuthForm />
-        </div>
-      </div>
-    </GoogleOAuthProvider>
+    <div className="min-h-screen bg-gray-950">
+      <Login />
+    </div>
   )
 }
 
